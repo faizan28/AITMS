a0424d7ce8f780e4492966ae8e56579b081312c7
anashussain93 <anas_hussain93@hotmail.com>
2016-12-25 19:39:32 +0500

    new login files has been added

---
+++ AIT_MS/Login.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace AIT_MS
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.lblLoginFailed = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUseranme = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chckboxAdminLogin = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.chckboxAdminLogin);
+            this.groupBox1.Controls.Add(this.btnRed);
+            this.groupBox1.Controls.Add(this.lblLoginFailed);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtUseranme);
+            this.groupBox1.Controls.Add(this.btnLogin);
+            this.groupBox1.Location = new System.Drawing.Point(12, 167);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(432, 175);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login";
+            this.groupBox1.Enter += new System.EventHandler(this.Login_Load);
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRed.ForeColor = System.Drawing.Color.Red;
+            this.btnRed.Location = new System.Drawing.Point(337, 160);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(21, 12);
+            this.btnRed.TabIndex = 6;
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // lblLoginFailed
+            // 
+            this.lblLoginFailed.AutoSize = true;
+            this.lblLoginFailed.Location = new System.Drawing.Point(364, 159);
+            this.lblLoginFailed.Name = "lblLoginFailed";
+            this.lblLoginFailed.Size = new System.Drawing.Size(64, 13);
+            this.lblLoginFailed.TabIndex = 5;
+            this.lblLoginFailed.Text = "Login Failed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(116, 108);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(163, 20);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtUseranme
+            // 
+            this.txtUseranme.Location = new System.Drawing.Point(116, 59);
+            this.txtUseranme.Name = "txtUseranme";
+            this.txtUseranme.Size = new System.Drawing.Size(163, 20);
+            this.txtUseranme.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(311, 59);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(53, 69);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(490, 39);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Aligarh Institute Of Technology";
+            // 
+            // chckboxAdminLogin
+            // 
+            this.chckboxAdminLogin.AutoSize = true;
+            this.chckboxAdminLogin.Location = new System.Drawing.Point(116, 136);
+            this.chckboxAdminLogin.Name = "chckboxAdminLogin";
+            this.chckboxAdminLogin.Size = new System.Drawing.Size(98, 17);
+            this.chckboxAdminLogin.TabIndex = 7;
+            this.chckboxAdminLogin.Text = "Login as Admin";
+            this.chckboxAdminLogin.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 354);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUseranme;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Label lblLoginFailed;
+        private System.Windows.Forms.CheckBox chckboxAdminLogin;
+    }
+}
---
+++ AIT_MS/Login.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace AIT_MS
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+       
+     
+        private void Login_Load(object sender, EventArgs e)
+        {
+            btnRed.Enabled = false;
+            btnRed.Visible = false;
+            lblLoginFailed.Visible = false;
+        }
+        
+        private void groupBox1_Enter(object sender, EventArgs e)
+        { 
+            
+            
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+
+            clsLogin objLogin = new clsLogin();
+            if (chckboxAdminLogin.Checked == true)
+            {
+                int adminId = objLogin.AdminLogin(txtUseranme.Text, txtPassword.Text);
+                if (adminId > 0)
+                {
+                    // this.Close();
+                    MessageBox.Show("Successfully Admin Logined");
+                }
+                else
+                {
+
+                    btnRed.Visible = true;
+                    lblLoginFailed.Visible = true;
+                    //chckboxAdminLogin.Visible = false;
+
+                }
+
+            }
+            else
+            {
+                int userId = objLogin.UserLogin(txtUseranme.Text, txtPassword.Text);
+                if (userId > 0)
+                {
+                    // this.Close();
+                    MessageBox.Show("Successfully Logined");
+                }
+                else
+                {
+
+                    btnRed.Visible = true;
+                    lblLoginFailed.Visible = true;
+
+                }
+            }
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
