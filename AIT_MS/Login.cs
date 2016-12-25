bfbbf8152d4047a212df2cd68dd4844298c68384
Faizan <faizan28@yahoo.com>
2016-12-25 19:49:17 +0500

    added comment for checking

--- AIT_MS/Login.cs
+++ AIT_MS/Login.cs
@@ -22,7 +22,7 @@ namespace AIT_MS
         {
             btnRed.Enabled = false;
             btnRed.Visible = false;
-            lblLoginFailed.Visible = false;
+            lblLoginFailed.Visible = false;//faizan
         }
         
         private void groupBox1_Enter(object sender, EventArgs e)
