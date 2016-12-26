945cd1d11772554aae9c7ffeababfe6b3bd2ea72
Faizan <faizan28@yahoo.com>
2016-12-26 17:10:46 +0500

    added register class clsRegister.cs

---
+++ AIT_MS/App_Code/clsRegister.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace AIT_MS.App_Code
+{
+    class clsRegister
+    {
+        DAL objDAl = new DAL();
+    }
+}
