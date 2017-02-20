67aaf7edb516f0552ff0232fd60608926dd9b7ca
Faizan <faizan28@yahoo.com>
2017-02-20 13:19:24 +0500

    Writeoff Entry Form has been Design

---
+++ AIT_MS/writeoffall/WriteoffEntry.Designer.cs
@@ -0,0 +1,417 @@
+﻿namespace AIT_MS.writeoffall
+{
+    partial class WriteoffEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dTPwoed = new System.Windows.Forms.DateTimePicker();
+            this.txtWqty = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.ComboBox();
+            this.txtrReason = new System.Windows.Forms.RichTextBox();
+            this.txtrRemarks = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.txtDept = new System.Windows.Forms.TextBox();
+            this.txtRoomNo = new System.Windows.Forms.TextBox();
+            this.txtCub = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtStaff = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtIssueID = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtItemName = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtMH = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtH = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtIqty = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dTPwoed
+            // 
+            this.dTPwoed.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dTPwoed.Location = new System.Drawing.Point(774, 16);
+            this.dTPwoed.Name = "dTPwoed";
+            this.dTPwoed.Size = new System.Drawing.Size(109, 20);
+            this.dTPwoed.TabIndex = 0;
+            // 
+            // txtWqty
+            // 
+            this.txtWqty.Location = new System.Drawing.Point(143, 19);
+            this.txtWqty.Name = "txtWqty";
+            this.txtWqty.Size = new System.Drawing.Size(100, 20);
+            this.txtWqty.TabIndex = 1;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.FormattingEnabled = true;
+            this.txtStatus.Location = new System.Drawing.Point(143, 54);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(121, 21);
+            this.txtStatus.TabIndex = 2;
+            // 
+            // txtrReason
+            // 
+            this.txtrReason.Location = new System.Drawing.Point(143, 94);
+            this.txtrReason.Name = "txtrReason";
+            this.txtrReason.Size = new System.Drawing.Size(218, 96);
+            this.txtrReason.TabIndex = 3;
+            this.txtrReason.Text = "";
+            // 
+            // txtrRemarks
+            // 
+            this.txtrRemarks.Location = new System.Drawing.Point(143, 210);
+            this.txtrRemarks.Name = "txtrRemarks";
+            this.txtrRemarks.Size = new System.Drawing.Size(218, 96);
+            this.txtrRemarks.TabIndex = 4;
+            this.txtrRemarks.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Quantity";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(388, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Status";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Reason";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Remarks";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(738, 22);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(30, 13);
+            this.lblDate.TabIndex = 10;
+            this.lblDate.Text = "Date";
+            // 
+            // txtDept
+            // 
+            this.txtDept.Location = new System.Drawing.Point(107, 74);
+            this.txtDept.Name = "txtDept";
+            this.txtDept.Size = new System.Drawing.Size(100, 20);
+            this.txtDept.TabIndex = 11;
+            // 
+            // txtRoomNo
+            // 
+            this.txtRoomNo.Location = new System.Drawing.Point(107, 113);
+            this.txtRoomNo.Name = "txtRoomNo";
+            this.txtRoomNo.Size = new System.Drawing.Size(100, 20);
+            this.txtRoomNo.TabIndex = 12;
+            // 
+            // txtCub
+            // 
+            this.txtCub.Location = new System.Drawing.Point(107, 147);
+            this.txtCub.Name = "txtCub";
+            this.txtCub.Size = new System.Drawing.Size(100, 20);
+            this.txtCub.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Department";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Room";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 154);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Cub";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(21, 195);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Staff";
+            // 
+            // txtStaff
+            // 
+            this.txtStaff.Location = new System.Drawing.Point(107, 188);
+            this.txtStaff.Name = "txtStaff";
+            this.txtStaff.Size = new System.Drawing.Size(100, 20);
+            this.txtStaff.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(21, 51);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "ISSUE ID";
+            // 
+            // txtIssueID
+            // 
+            this.txtIssueID.Location = new System.Drawing.Point(107, 44);
+            this.txtIssueID.Name = "txtIssueID";
+            this.txtIssueID.Size = new System.Drawing.Size(100, 20);
+            this.txtIssueID.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(14, 134);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Item Name";
+            // 
+            // txtItemName
+            // 
+            this.txtItemName.Location = new System.Drawing.Point(100, 127);
+            this.txtItemName.Name = "txtItemName";
+            this.txtItemName.Size = new System.Drawing.Size(100, 20);
+            this.txtItemName.TabIndex = 21;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(14, 40);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(68, 13);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Master Head";
+            // 
+            // txtMH
+            // 
+            this.txtMH.Location = new System.Drawing.Point(100, 33);
+            this.txtMH.Name = "txtMH";
+            this.txtMH.Size = new System.Drawing.Size(100, 20);
+            this.txtMH.TabIndex = 23;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(14, 88);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(33, 13);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "Head";
+            // 
+            // txtH
+            // 
+            this.txtH.Location = new System.Drawing.Point(100, 81);
+            this.txtH.Name = "txtH";
+            this.txtH.Size = new System.Drawing.Size(100, 20);
+            this.txtH.TabIndex = 25;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(14, 178);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(46, 13);
+            this.label13.TabIndex = 28;
+            this.label13.Text = "Quantity";
+            // 
+            // txtIqty
+            // 
+            this.txtIqty.Location = new System.Drawing.Point(100, 178);
+            this.txtIqty.Name = "txtIqty";
+            this.txtIqty.Size = new System.Drawing.Size(100, 20);
+            this.txtIqty.TabIndex = 27;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtStaff);
+            this.groupBox1.Controls.Add(this.txtDept);
+            this.groupBox1.Controls.Add(this.txtRoomNo);
+            this.groupBox1.Controls.Add(this.txtCub);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtIssueID);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Location = new System.Drawing.Point(12, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 236);
+            this.groupBox1.TabIndex = 29;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ISSUE DETAILS";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Controls.Add(this.txtItemName);
+            this.groupBox2.Controls.Add(this.txtIqty);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.txtMH);
+            this.groupBox2.Controls.Add(this.txtH);
+            this.groupBox2.Location = new System.Drawing.Point(253, 47);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(238, 236);
+            this.groupBox2.TabIndex = 30;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "ITEM DETAILS";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtWqty);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.txtStatus);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.txtrReason);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.txtrRemarks);
+            this.groupBox3.Location = new System.Drawing.Point(504, 47);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(380, 319);
+            this.groupBox3.TabIndex = 31;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "WRITE OFF DETAILS";
+            // 
+            // WriteoffEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 445);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dTPwoed);
+            this.Name = "WriteoffEntry";
+            this.Text = "WriteoffEntry";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dTPwoed;
+        private System.Windows.Forms.TextBox txtWqty;
+        private System.Windows.Forms.ComboBox txtStatus;
+        private System.Windows.Forms.RichTextBox txtrReason;
+        private System.Windows.Forms.RichTextBox txtrRemarks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.TextBox txtDept;
+        private System.Windows.Forms.TextBox txtRoomNo;
+        private System.Windows.Forms.TextBox txtCub;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtStaff;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtIssueID;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtItemName;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtMH;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtH;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtIqty;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
